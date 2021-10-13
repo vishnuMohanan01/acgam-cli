4908457d08f36e19f83cb37218b15a91a33530c2
Iam-VM <vishnumohanantheking@gmail.com>
2021-10-13 23:38:30 +0530

    added certs store creation func

--- utils/make_dirs_and_files.py
+++ utils/make_dirs_and_files.py
@@ -1,2 +1,13 @@
-def make_gen_certs_dir():
-    pass
+import os
+
+
+def make_gen_certs_dir(gen_certs_dir, event_name):
+    if not os.path.isfile(os.path.join(gen_certs_dir)):
+        os.mkdir(os.path.join(gen_certs_dir))
+
+    from datetime import datetime
+    curr_time_stamp = datetime.now().strftime("%d-%b-%Y_(%H:%M:%S)")
+    certs_store_dir = f"{event_name}_{curr_time_stamp}"
+    os.mkdir(os.path.join(gen_certs_dir, certs_store_dir))
+
+    return certs_store_dir
