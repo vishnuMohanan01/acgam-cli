aae2533c8f8c06ac3b145f84034591eb1f084688
Iam-VM <vishnumohanantheking@gmail.com>
2021-10-21 21:32:37 +0530

    added args

--- utils/arg_parse.py
+++ utils/arg_parse.py
@@ -4,18 +4,54 @@ import argparse
 def init_arg_parser():
     arg_parser = argparse.ArgumentParser(prog='acgam-cli',
                                          usage='python3 main.py [options] [value]',
-                                         description='generates certificates for given recipients and mails them')
-    arg_parser.add_argument('-t',
-                            '--template',
+                                         description='Generates and mails certificates for given recipients.')
+    # TODO: add this feature
+    arg_parser.add_argument('-a',
+                            '--add-event',
                             action='store',
-                            required=True,
+                            # required=True,
+                            help='To add an event  to DB. Specify the event name as value. Returns event ID.')
+    arg_parser.add_argument('-f',
+                            '--file',
+                            action='store',
+                            # required=True,
+                            help='To specify the CSV file path.')
+    # TODO: add this feature
+    arg_parser.add_argument('-g',
+                            '--get-event-id',
+                            action='store',
+                            # required=True,
+                            help='Specify the event name.')
+    # TODO: Replace this feature with --event-id
+    # Temp Feature
+    arg_parser.add_argument('-E',
+                            '--event-id',
+                            action='store',
+                            # required=True,
+                            help='Specify the event id.')
+    # TODO: add this feature
+    arg_parser.add_argument('-E',
+                            '--event-id',
+                            action='store',
+                            # required=True,
+                            help='Specify the event id.')
+
+    arg_parser.add_argument('-O',
+                            '--organization',
+                            action='store',
+                            # required=True,
                             choices=["comsoc", "cs", "ias", "sb", "wie"],
-                            help='specify the name of the certificate template to be used')
-    arg_parser.add_argument('-z',
-                            '--zone-name',
+                            help='Specify the name of the organization issuing the certificate.')
+    arg_parser.add_argument('-T',
+                            '--type',
                             action='store',
-                            required=True,
-                            help='Takes in name of the zone.')
+                            choices=["p", "participants", "w", "winners"],
+                            # required=True,
+                            help='Specify the type of the certificate being issued.')
+    arg_parser.add_argument('-N',
+                            '--no-mail',
+                            action='store_true',
+                            help='Use this flag if generated certificates need not be mailed to the recipients.')
 
     return arg_parser
 
