9d9f7797bedc966fd0541cfb71c83199cee7bc9e
Iam-VM <vishnumohanantheking@gmail.com>
2021-11-19 06:10:49 +0530

    renamed cert_gen_dir

--- constants.py
+++ constants.py
@@ -1,3 +1,20 @@
+# PATHS
 FONT_DIR = "fileSystem/fonts"
-GENERATED_CERTS_DIR = "fileSystem/generatedCerts"
+CERT_STORE_DIR = "fileSystem/generatedCerts"
 TEMPLATE_DIR = "fileSystem/certTemplates"
+
+# These comes on certs and mails
+ORG_FULL_NAMES = {
+    "comsoc": "IEEE ComSoc SBC GEC Palakkad",
+    "cs": "IEEE CS SBC GEC Palakkad",
+    "ias": "IEEE IAS SBC GEC Palakkad",
+    "sb": "IEEE SB GEC Palakkad",
+    "wie": "IEEE WIE AG GEC Palakkad"
+}
+
+CERT_TITLE = {
+    "c": "Coordinator Certificate",
+    "p": "Certificate Of Participation",
+    "v": "Volunteer Certificate",
+    "w": "Certificate of Achievement"
+}
