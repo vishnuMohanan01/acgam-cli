7753b8c48a7f87700fdc422b143eb9196f8df9d2
Iam-VM <vishnumohanantheking@gmail.com>
2021-11-20 17:40:24 +0530

    resturctured prog options and updated with constants

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -7,41 +7,23 @@ class ArgParser:
         self.program_name = os.environ["PROGRAM_NAME"]
 
     def __init_arg_parser(self):
+        from .. import constants
+
         arg_parser = argparse.ArgumentParser(prog=self.program_name,
                                              usage='python3 main.py [options] [value]',
                                              description='Generates and mails certificates for given recipients.')
-        # TODO: add this feature
-        arg_parser.add_argument('-a',
-                                '--add-event',
-                                action='store',
-                                # required=True,
-                                help='To add an event  to DB. Specify the event name as value. Returns event ID.')
         arg_parser.add_argument('-d',
                                 '--date',
                                 action='store',
                                 # required=True,
                                 help='To specify the event date.')
         arg_parser.add_argument('-f',
-                                '--file',
+                                '--csv-file',
                                 action='store',
                                 # required=True,
                                 help='To specify the CSV file path.')
-        # TODO: add this feature
-        arg_parser.add_argument('-g',
-                                '--get-event-id',
-                                action='store',
-                                # required=True,
-                                help='Specify the event name.')
-        # TODO: Replace this feature with --event-id
-        # Temp Feature
         arg_parser.add_argument('-e',
                                 '--event-name',
-                                action='store',
-                                # required=True,
-                                help='Specify the event id.')
-        # TODO: add this feature
-        arg_parser.add_argument('-E',
-                                '--event-id',
                                 action='store',
                                 # required=True,
                                 help='Specify the event id.')
@@ -49,25 +31,47 @@ class ArgParser:
                                 '--organization',
                                 action='store',
                                 # required=True,
-                                choices=["comsoc", "cs", "ias", "sb", "wie"],
                                 help='Specify the name of the organization issuing the certificate.')
         arg_parser.add_argument('-N',
                                 '--no-mail',
                                 action='store_true',
-                                help='Use this flag if generated certificates need not be mailed to the recipients.')
-        arg_parser.add_argument('-T',
-                                '--type',
+                                default=True,
+                                help='Use this flag if generated certificates need not be mailed to the recipients. [Default]')
+        arg_parser.add_argument('-r',
+                                '--recipient-type',
                                 action='store',
-                                choices=["p", "participants", "w", "winners"],
+                                choices=constants.RECIPIENT_TYPES,
                                 # required=True,
                                 help='Specify the type of the certificate being issued.')
+        arg_parser.add_argument('-t',
+                                '--test',
+                                action='store_true',
+                                # required=True,
+                                help='Use this flag for test runs. Generates certs, no mail, no logging to DB.')
+        # Unimplemented
+        # TODO: add these features
+        arg_parser.add_argument('-a',
+                                '--add-event',
+                                action='store',
+                                # required=True,
+                                help='To add an event  to DB. Specify the event name as value. Returns event ID.')
+        arg_parser.add_argument('-E',
+                                '--event-id',
+                                action='store',
+                                # required=True,
+                                help='Specify the event id.')
+        arg_parser.add_argument('-g',
+                                '--get-event-id',
+                                action='store',
+                                # required=True,
+                                help='Specify the event name.')
         return arg_parser
 
     def get_args(self):
         args = self.__init_arg_parser().parse_args()
         return {
             "org_name": args.organization,
-            "cert_type": args.type,
+            "recipient_type": args.recipient_type,
             "event_name": args.event_name,
             "csv_path": args.file,
             "event_date": args.date
