f7b33c0f3761b0371daee15b4691e96ca4dd160f
Iam-VM <vishnumohanantheking@gmail.com>
2021-12-29 11:40:25 +0530

    renamed some constants

--- constants.py
+++ constants.py
@@ -4,20 +4,22 @@ CERT_STORE_DIR = "fileSystem/generatedCerts"
 TEMPLATE_DIR = "fileSystem/certTemplates"
 
 # Other vars
-RECIPIENT_TYPES = ["c", "coordinators",
-                   "p", "participants",
-                   "v", "volunteers",
-                   "w", "winners"]
+RECIPIENT_TYPES = [
+    "coordinators",
+    "participants",
+    "volunteers",
+    "winners"
+]
 
-ORG_FULL_NAMES = {
-    "comsoc": "IEEE ComSoc SBC GEC Palakkad",
-    "cs": "IEEE CS SBC GEC Palakkad",
-    "ias": "IEEE IAS SBC GEC Palakkad",
-    "sb": "IEEE SB GEC Palakkad",
-    "wie": "IEEE WIE AG GEC Palakkad"
-}
+ORG_NAMES = [
+    "IEEE ComSoc SBC GEC Palakkad",
+    "IEEE CS SBC GEC Palakkad",
+    "IEEE IAS SBC GEC Palakkad",
+    "IEEE SB GEC Palakkad",
+    "IEEE WIE AG GEC Palakkad"
+]
 
-CERT_TITLE = {
+CERT_TITLES = {
     "c": "Coordinator Certificate",
     "p": "Certificate Of Participation",
     "v": "Volunteer Certificate",
